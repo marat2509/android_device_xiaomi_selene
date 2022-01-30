43d43bdd7854b933480f15b5a3817874292ce6a3
marat2509-hub <93652988+marat2509-hub@users.noreply.github.com>
2022-01-30 17:35:17 +0300

    selene: Include ANXCamera [2/2]

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,6 +52,9 @@ BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 BUILD_BROKEN_PREBUILT_ELF_FILES := true
 
+# ANXCamera
+-include vendor/aeonax/ANXCamera/BoardConfigAnx.mk
+
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := selene
 TARGET_NO_BOOTLOADER := true
